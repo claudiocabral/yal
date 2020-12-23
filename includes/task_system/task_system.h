7a1376d989b2ce75d36aee49935ad2c854182fec
Claudio Cabral <cabral1349@gmail.com>
2020-12-23 23:02:07 +0100

    exit task system if started with no tasks

--- includes/task_system/task_system.h
+++ includes/task_system/task_system.h
@@ -9,27 +9,29 @@ struct TaskSystem {
     unsigned count{std::thread::hardware_concurrency()};
     std::vector<TaskQueue> queues{count};
     std::vector<std::thread> threads;
-    uint8_t current = 0;
+    int current = 0;
     std::atomic<int> tasks = 0;
     bool done = false;
 
     void spawn(TaskQueue::Task && task) {
-        ++tasks;
+        tasks.fetch_add(1, std::memory_order_release);
         auto i = current++;
         auto size = queues.size();
         while (true) {
             if (done || queues[i++ % size].try_push(std::move(task))) return;
         }
     }
-    void runTask(uint8_t threadId) {
+    void runTask(int threadId) {
         auto size = queues.size();
+        int i = threadId;
+        if (tasks == 0) return;
         while (true) {
             if (done) return;
-            auto task = queues[threadId++ % size].try_pop();
+            auto task = queues[i++ % size].try_pop();
             if (task) {
                 (*task)();
-                --tasks;
-                if (tasks == 0) done = true;
+                auto remaningTasks = tasks.fetch_sub(1, std::memory_order_release) - 1;
+                if (remaningTasks <= 0) done = true;
             }
         }
     }
